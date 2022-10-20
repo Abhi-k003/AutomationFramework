1c9fc769e028c2e83d5a9e4f7162e0ebd641aa53
ADMIN <ADMIN@DESKTOP-QR0VDMM>
2022-10-20 06:57:06 +0530

    commit and push by user 1-Flipkart

---
+++ src/test/java/sample/Tests/FlipkartTest.java
@@ -0,0 +1,24 @@
+package sample.Tests;
+
+import org.testng.annotations.Test;
+
+public class FlipkartTest 
+{
+	@Test
+	public void login()
+	{
+		System.out.println("login succesfull");
+	}
+	
+	@Test
+	public void addToCart()
+	{
+		System.out.println("add to cart is succesfull");
+	}
+	@Test
+	public void removeFromCart()
+	{
+		System.out.println("remove from cart succesfull");
+	}
+
+}
